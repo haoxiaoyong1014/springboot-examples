edd3ab512039fe06d451f36476eb0e8b0cb94f1c
haoxiaoyong1014 <hxyHelloWorld@163.com>
2018-06-13 15:18:02 +0800

    add springboot-web-thymeleaf model

---
+++ springboot-web-thymeleaf/src/main/java/com/haoxy/thymeleaf/model/Message.java
@@ -0,0 +1,9 @@
+package com.haoxy.thymeleaf.model;
+
+/**
+ * Created by hxy on 2018/6/13.
+ * E-mail:hxyHelloWorld@163.com
+ * github:https://github.com/haoxiaoyong1014
+ */
+public class Message {
+}
