3545cf9f7df9cdaa6163c172aaacba95a9cbf102
haoxiaoyong1014 <hxyHelloWorld@163.com>
2018-06-29 14:20:15 +0800

    add springboot-mybaits-ehcache

--- springboot-mybatis-ehcache/src/main/java/com/haoxy/example/AppEhcache.java
+++ springboot-mybatis-ehcache/src/main/java/com/haoxy/example/AppEhcache.java
@@ -8,9 +8,11 @@ import org.springframework.context.annotation.ImportResource;
  * Created by hxy on 2018/6/27.
  * E-mail:hxyHelloWorld@163.com
  * github:https://github.com/haoxiaoyong1014
+ *
  */
 @SpringBootApplication
 public class AppEhcache {
+
     public static void main(String[] args) {
         SpringApplication.run(AppEhcache.class, args);
     }
