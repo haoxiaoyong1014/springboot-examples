a39b8f9474dd7e6480954b124693ddcaa3dc6e3a
haoxiaoyong1014 <hxyHelloWorld@163.com>
2019-05-29 17:34:40 +0800

    add Interceptor

--- springboot-login-Interceptor/src/main/java/cn/haoxy/interceptor/config/AuthenticationInterceptor.java
+++ springboot-login-Interceptor/src/main/java/cn/haoxy/interceptor/config/AuthenticationInterceptor.java
@@ -1,10 +1,15 @@
 package cn.haoxy.interceptor.config;
 
+import cn.haoxy.interceptor.annotation.LoginRequired;
+import cn.haoxy.interceptor.utils.TokenUtils;
+import io.jsonwebtoken.Claims;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
 
 /**
  * Created by Haoxy on 2019-05-29.
@@ -13,8 +18,31 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class AuthenticationInterceptor implements HandlerInterceptor {
 
+
     @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object handler) throws Exception {
+        //如果不是映射到方法就不需要拦截
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+        //得到请求的哪个方法
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+        //判断是否这个接口是否需要登录才能访问
+        LoginRequired annotation = method.getAnnotation(LoginRequired.class);
+        if (annotation != null) {
+            //这个方法上含有这个注解,说明需要登录才能请求
+            String token = httpServletRequest.getHeader("token");
+            if (token == null) {
+                throw new RuntimeException("无token，请重新登录");
+            }
+            Claims claims = TokenUtils.parseJWT(token);
+            String UserId = claims.getId();
+        } else {
+            return true;
+        }
+
+
         return false;
     }
 
