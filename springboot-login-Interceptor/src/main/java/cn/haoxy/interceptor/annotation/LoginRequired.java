74a7e212397e34518f523b883c36bd3f94454754
haoxiaoyong1014 <hxyHelloWorld@163.com>
2019-05-29 17:19:37 +0800

    add Interceptor

--- springboot-login-Interceptor/src/main/java/cn/haoxy/interceptor/annotation/LoginRequired.java
+++ springboot-login-Interceptor/src/main/java/cn/haoxy/interceptor/annotation/LoginRequired.java
@@ -1,9 +1,15 @@
 package cn.haoxy.interceptor.annotation;
-
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 /**
  * Created by Haoxy on 2019-05-29.
  * E-mail:hxyHelloWorld@163.com
  * github:https://github.com/haoxiaoyong1014
  */
+//在需要登录验证的Controller的方法上使用此注解
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface LoginRequired {
 }
--- springboot-login-Interceptor/src/main/java/cn/haoxy/interceptor/inter/AuthenticationInterceptor.java
+++ springboot-login-Interceptor/src/main/java/cn/haoxy/interceptor/inter/AuthenticationInterceptor.java
@@ -1,9 +1,30 @@
 package cn.haoxy.interceptor.inter;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by Haoxy on 2019-05-29.
  * E-mail:hxyHelloWorld@163.com
  * github:https://github.com/haoxiaoyong1014
  */
-public class AuthenticationInterceptor {
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
 }
